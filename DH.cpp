faaa4913a1de388907a20ce8b8b24deea1937d39
Offliner <42777325+Offliners@users.noreply.github.com>
2021-07-30 00:21:27 +0800

    Add files via upload

--- DH.cpp
+++ DH.cpp
@@ -3,87 +3,86 @@
 #include<fstream>
 #include<string>
 #include"Manipulator.h"
-#include"argParser.h"
+#include"ArgParser.h"
 
 using namespace std;
 
 void errorMsg(); 
 bool checkValid(const string&, int);
 
+class DH_ArgParser : ArgParser
+{
+    public:
+        DH_ArgParser(int argc, char **argv, const std::string& name, const std::vector< std::vector<std::string> > &arg) : ArgParser(name, arg)
+        {
+            if(argc > 1)
+                all_args_.assign(argv + 1, argv + argc);
+            else
+            {
+                this->errorMsg();
+                std::exit(-1);
+            }
+        };
+
+        ~DH_ArgParser()
+        {
+            all_args_.clear();
+        };
+
+        void readFileAndShowTF()
+        {
+            const string filename = all_args_[1];
+            ifstream DH_file(filename);
+            if(!DH_file)
+            {
+                cout << "\"" << filename << "\"" << "File can't be opened! Please check the path of the file!" << endl;
+                return;
+            }
+
+            int cnt = 1;
+            int alpha, a, d, theta;
+            Manipulator m;
+            while(DH_file >> alpha >> a >> d >> theta)
+            {
+                m.addOneJoint(cnt, alpha, a, d, theta);
+                ++cnt;
+            }
+            m.ShowTransformMatrix(atoi(all_args_[2].c_str()), atoi(all_args_[3].c_str()));
+        };
+
+        void argFunction()
+        {
+            int temp = this->isArgValid(all_args_);
+            switch(temp)
+            {
+                case -1:
+                    this->errorMsg();
+                    break;
+                case 0:
+                    this->help();
+                    break;
+                case 1:
+                    this->readFileAndShowTF();
+            }
+        };
+
+        private:
+            std::vector< std::string > all_args_;
+};
+
 int main(int argc, char **argv)
 {
-    vector< pair<string, string> > opt =
+    vector< vector<string> > opt
     {
-        {"-h", "Print help and exit"},
-        {"-f", "File path of DH table (Please use txt file to record the table)"}
+        {"-h", "--help", "2", "Print help and exit"},
+        {"-f", "--file", "5", "File path of DH table (Please use txt file to record the table)"}
     };
 
     string tool_name = argv[0];
     vector<string> all_args;
-    ArgParser arg("DH", opt);
+    DH_ArgParser arg(argc, argv, tool_name.erase(0, 2), opt);
     
-    if(argc > 1 && arg.optIndex(string(argv[1])) != -1)
-    {
-        all_args.assign(argv + 1, argv + argc);
-        
-        if(all_args[0] == "-h")
-        {
-            if(argc != 2)
-                errorMsg();
-            else
-                arg.help();
-        }
-        else if(all_args[0] == "-f")
-        {
-            if(argc != 5)
-                errorMsg();
-            else
-            {
-                const string filename = all_args[1];
-                ifstream DH_file(filename);
-                if(!DH_file)
-                {
-                    cout << "\"" << filename << "\"" << "File can't be opened! Please check the path of the file!" << endl;
-                    return 0;
-                }
-
-                int cnt = 1;
-                int alpha, a, d, theta;
-                Manipulator m;
-                while(DH_file >> alpha >> a >> d >> theta)
-                {
-                    m.addOneJoint(cnt, alpha, a, d, theta);
-                    ++cnt;
-                }
-                
-                if(checkValid(all_args[2], cnt) && checkValid(all_args[3], cnt))
-                    m.ShowTransformMatrix(atoi(all_args[2].c_str()), atoi(all_args[3].c_str()));
-                else
-                    errorMsg();
-            }
-        }
-        else
-            errorMsg();
-    }
-    else
-        errorMsg();
+    arg.argFunction();
 
     return 0;
-}
-
-void errorMsg()
-{
-    cout << "Error usage! Please use [-h] to check correct arguments" << endl;
-}
-
-bool checkValid(const string& str, int maxNum)
-{
-    for (char const &c : str)
-        if (isdigit(c) == 0) 
-            return false;
-    
-    if(atoi(str.c_str()) > maxNum)
-        return false;
-
-    return true;
 }
